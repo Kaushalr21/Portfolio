565d736a64cdc96756610d54f471212f8a3ff8f7
Kaushalr21 <kaushalram021@gmail.com>
2025-02-07 23:26:08 +0530

    good ui

--- src/App.jsx
+++ src/App.jsx
@@ -1,30 +1,36 @@
-import React from 'react';
+import React, { useState } from "react";
 import './App.css'; // Import your CSS file for styling   <img src="bg.jpg" alt="Profile" className="profile-photo"/>
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
   return (
     <div className="container-fluid">
       {/* First Div: Profile Photo and Navbar */}
       
-      <div className="profile-section box">
+      <div id="ok" className="profile-section box">
         <div className='pro-text container'>
           <div className='pro-text1'>
-            <h1>Hello, I am</h1>
+            <h1 >Hello, I am</h1>
             <h1 className='name'><b>Kaushal R</b></h1>
             <h4>Front-end Designer | Developer</h4>
           </div>
-          <img className="pro-photo" src="wew" alt="" />
+          <img  className="pro-photo" src="" alt="hjh" />
         </div>
         
       </div>
       <nav className="navbar">
-          <div className="navbarL">
+          <div className="moreIcon" onClick={() => setIsActive(!isActive)} >
+              <h1><i class="bi bi-list"></i></h1>
+              <h1>Kaushal R</h1>
+          </div>
+          <div className={`navbarL ${isActive ? "active" : ""}`}>
             <ul>
-              <li><a href="#home">Home</a></li>
+              <li><a href="#ok">Home</a></li>
               <li><a href="#home">About</a></li>
               <li><a href="#education">Education</a></li>
               
             </ul>
+            <h3>R Kaushal</h3>
             <ul>
               <li><a href="#skillsets">Skillsets</a></li>
               <li><a href="#projects">Projects</a></li>
@@ -36,7 +42,7 @@ function App() {
 
       {/* Education Section */}
       <div id="education" className="section">
-        <div className='data'>           
+        <div  className='data'>           
           <h1>Who am I?</h1>
           <p>I'm a web developer creating seamless,optimizing websites and user-friendly digital experiences. </p>
         </div>
@@ -57,7 +63,6 @@ function App() {
 
       {/* Skillsets Section */}
       <div id="skillsets" className="section">
-        <h2>Skillsets</h2>
         <div className='data'>           
           <p><b>Web Development</b></p>
           <p>JavaScript, ReactJS, HTML, CSS</p>
--- src/main.jsx
+++ src/main.jsx
@@ -5,7 +5,6 @@ import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <h1 className='header'>Portfolio</h1>
     <App />
     <footer className="footer">
         <p>&copy; 2025 Kaushal. All rights reserved.</p>
